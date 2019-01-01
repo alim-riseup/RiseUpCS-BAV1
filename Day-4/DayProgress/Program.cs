c8b7fcaf4fa2af3316fbf693456c6794518bde38
Md. Alim Ul karim <alim.karim@aurea.com>
2019-01-02 00:06:33 +0600

    - added new project and added a single function

---
+++ Day-4/DayProgress/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayProgress
+{
+    class Program
+    {
+        static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        static void Main(string[] args)
+        {
+            PrintHelloWorld();
+
+            Console.ReadLine();
+        }
+    }
+}
